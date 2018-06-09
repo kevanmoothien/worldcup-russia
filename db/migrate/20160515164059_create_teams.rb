1d54293105cc71841829795401f5b84a54258ea0
kevanmoothien <kevan.moothien@spoonconsulting.com>
2018-06-09 13:45:07 +0400

    uuid

--- db/migrate/20160515164059_create_teams.rb
+++ db/migrate/20160515164059_create_teams.rb
@@ -1,5 +1,6 @@
 class CreateTeams < ActiveRecord::Migration[4.2]
   def change
+    enable_extension 'uuid-ossp'
     create_table :teams, id: :uuid do |t|
       t.string :name
       t.string :api_name
--- db/migrate/20160515164536_enable_uuid_ossp_extension.rb
+++
@@ -1,5 +0,0 @@
-class EnableUuidOsspExtension < ActiveRecord::Migration[4.2]
-  def change
-    enable_extension 'uuid-ossp'
-  end
-end
