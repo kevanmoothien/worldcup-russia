7c9f91adde96cd6eef8a1e533b836a11396b76c3
kevanmoothien <kevan.moothien@spoonconsulting.com>
2018-06-09 13:55:35 +0400

    fix column

--- db/migrate/20160516185211_create_matches.rb
+++ db/migrate/20160516185211_create_matches.rb
@@ -1,8 +1,8 @@
 class CreateMatches < ActiveRecord::Migration[4.2]
   def change
     create_table :matches, id: :uuid do |t|
-      t.integer :team_a
-      t.integer :team_b
+      t.uuid :team_a
+      t.uuid :team_b
       t.integer :score_a
       t.integer :score_b
       t.datetime :schedule
--- db/migrate/20160518171254_create_bets.rb
+++ db/migrate/20160518171254_create_bets.rb
@@ -4,7 +4,7 @@ class CreateBets < ActiveRecord::Migration[4.2]
       t.integer :score_a
       t.integer :score_b
       t.integer :user_id
-      t.integer :match_id
+      t.uuid :match_id
       t.uuid :public_id, default: 'uuid_generate_v4()'
 
       t.timestamps null: false
